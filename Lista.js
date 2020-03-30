c3296231c2529c10bf7576a3d24fe12d81babadf
HiCompany5680 <45556227+HiCompany5680@users.noreply.github.com>
2020-03-30 12:59:14 -0500

    Create Lista.js

---
+++ Lista.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Lista extends Component {
+
+    render() {
+
+        return (
+            <div className="lista">
+                <ul>
+                    <li>Futbol</li>
+                    <li>Cajica</li>
+                    <li>Colombia</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Lista;
